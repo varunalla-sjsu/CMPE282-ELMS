bc6797cc4a0a45a43982c59ec605d211913d8165
Raj Kinkhabwala <kinkhabwalaraj@gmail.com>
2022-04-19 18:47:55 -0700

    edited to showcase changes

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React and Github actions
         </a>
       </header>
     </div>
