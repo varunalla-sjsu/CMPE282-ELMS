d7fee2e4b16dbf448be4a5824bb00acbc5caa144
Varun Alla <varun.alla@sjsu.edu>
2022-04-22 22:47:00 -0700

    updated backend

--- backend/IaC/ecr/main.tf
+++ backend/IaC/ecr/main.tf
@@ -5,6 +5,12 @@ terraform {
       source  = "hashicorp/aws"
     }
   }
+  backend "s3" {
+    bucket  = "elmsterraform"
+    key     = "ecr-state/states"
+    region  = "us-west-1"
+    encrypt = true
+  }
 }
 provider "aws" {
   region = "us-west-1"
