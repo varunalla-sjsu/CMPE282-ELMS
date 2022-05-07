1dee93f39fa48d5d573a2dc625a056a11c537025
Varun Alla <varun.alla@sjsu.edu>
2022-05-07 00:45:37 -0700

    added prisma config

--- backend/elms-backend/src/app.controller.ts
+++ backend/elms-backend/src/app.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 import { ApiStatus } from './models/ApiStatus';
+import { PrismaService } from './services/prisma.service';
 
 @Controller()
 export class AppController {
--- backend/elms-backend/src/main.ts
+++ backend/elms-backend/src/main.ts
@@ -1,12 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
+import { PrismaService } from './services/prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors();
   app.setGlobalPrefix('api');
   console.log(process.env);
+  const prismaService = app.get(PrismaService);
+  await prismaService.enableShutdownHooks(app);
   await app.listen(80);
 }
 bootstrap();
