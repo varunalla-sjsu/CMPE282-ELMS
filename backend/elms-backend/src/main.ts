ad4016009e2a0e122b689b991511547d0f7a59ec
Varun Alla <varun.alla@sjsu.edu>
2022-05-07 00:33:58 -0700

    innject variable via ecs fargate

--- backend/elms-backend/src/main.ts
+++ backend/elms-backend/src/main.ts
@@ -6,6 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors();
   app.setGlobalPrefix('api');
+  console.log(process.env);
   await app.listen(80);
 }
 bootstrap();
