1ce369d705a66e9ca04696b57197b1b4b7e1f3ce
Varun Alla <varun.alla@sjsu.edu>
2022-05-13 01:35:58 -0700

    swagger integrated

--- backend/elms-backend/src/main.ts
+++ backend/elms-backend/src/main.ts
@@ -2,12 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 import { PrismaService } from './services/prisma.service';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors();
   app.setGlobalPrefix('api');
   //console.log(process.env);
+  const config = new DocumentBuilder().setTitle('ELMS API').setDescription('API Description for ELMS').setVersion('1.0').addTag('ELMS').build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
   const prismaService = app.get(PrismaService);
   await prismaService.enableShutdownHooks(app);
   await app.listen(80);
